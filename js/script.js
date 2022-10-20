5e96b8cb460d48a661274a0d05cc3e4288ff5c00
creativedaz <it.vincentleroy.vl@gmail.com>
2022-10-20 09:25:13 +0800

    init
    added animation to mobile nav & trying to fix the background-clip for iOS

--- js/script.js
+++ js/script.js
@@ -1,12 +1,15 @@
-// HAMBURGER MENU
-var hamburger = document.getElementById("hamburger");
-var navUL = document.getElementById("nav_ul");
+$(document).ready(function(){
+  // HAMBURGER MENU
+  var hamburger = document.getElementById("hamburger");
+  var navUL = document.getElementById("nav_ul");
 
-$(hamburger).click(function() {
-  navUL.classList.toggle("show");
-});
+  $("#hamburger").click(function() {
+    // navUL.classList.toggle("show");
+    $("#nav_ul").slideToggle("show");
+  });
 
-// STICKY NAVBAR
-jQuery(window).scroll(function () {
-  jQuery(".header_nav").toggleClass("fixed", jQuery(window).scrollTop() > 50);
+  // STICKY NAVBAR
+  jQuery(window).scroll(function () {
+    jQuery(".header_nav").toggleClass("fixed", jQuery(window).scrollTop() > 50);
+  });
 });
